48360c2b2af3a4df248e082952721203e8e922df
jmoore <josh@glencoesoftware.com>
2018-09-05 11:06:09 +0100

    Remove sptx-format

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source activate env
-set -euxo pipefail
-
-mkdir /tmp/in /tmp/out
-
-cd starfish
-python examples/get_iss_data.py --d=true /tmp/in /tmp/out
-validate-sptx --experiment-json /tmp/out/experiment.json
--- test2.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source activate env
-set -euxo pipefail
-
-mkdir /tmp/in /tmp/out
-
-starfish build --fov-count=2 --hybridization-dimensions='{"z": 3}' /tmp
-validate-sptx --experiment-json /tmp/experiment.json
