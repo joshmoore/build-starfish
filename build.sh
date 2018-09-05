ddf29d5f069d3b4edc5c71f9ae13c05242ed752f
jmoore <josh@glencoesoftware.com>
2018-09-05 11:44:29 +0100

    Build requirements-dev for jsonpath_rw

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 LOG=$(basename $PWD).log
 
 echo "REQUIREMENTS" >> $LOG
-pip install -r $(find . -iname requirements.txt) | tee -a $LOG
+pip install -r $(find . -iname requirements-dev.txt) | tee -a $LOG
 echo "INSTALL" >> $LOG
 pip install . | tee -a $LOG
